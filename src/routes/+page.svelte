829129b4fc46e65ebf2cc4feadbb0b1680917889
Daniel Cui <solunian@gmail.com>
2025-07-31 02:10:12 -0700

    feat: basic websockets chat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let inputbox = $state("");
+  let messages: string[] = $state([]);
+
+  let ws = new WebSocket("ws://localhost:8000/ws/");
+
+  // ws.onopen = () => {
+  //   console.log("WebSocket open");
+  // };
+
+  ws.onmessage = (event) => {
+    messages.push(event.data);
+  };
+
+  // ws.onerror = (error) => {
+  //   console.error("WebSocket error:", error);
+  // };
+
+  // ws.onclose = () => {
+  //   console.log("WebSocket close");
+  // };
+</script>
+
+<div class="spaces-y-4 px-2">
+  <h1 class="text-3xl text-orange-500">WebSocket Chat</h1>
+  <form
+    onsubmit={(event) => {
+      event.preventDefault();
+      ws.send(inputbox);
+      inputbox = "";
+    }}>
+    <div class="flex flex-row gap-2">
+      <input type="text" bind:value={inputbox} class="w-full border px-2" autocomplete="off" />
+      <button class="border px-2">Send</button>
+    </div>
+  </form>
+  <ul>
+    {#each messages as m}
+      <li>{m}</li>
+    {/each}
+  </ul>
+</div>
